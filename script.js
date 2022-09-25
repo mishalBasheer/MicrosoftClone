71e4d6af964b53345f2b9d08da048c9285518d40
Mohammed Mishal <mishal2mk2@gmail.com>
2022-09-26 05:29:38 +0530

    complete btt button responsive screen

--- script.js
+++ script.js
@@ -5,12 +5,33 @@ let mybutton = document.getElementById("btt");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if ((document.body.scrollTop > 200 && document.body.scrollTop < 1880)|| (document.documentElement.scrollTop > 200 && document.documentElement.scrollTop < 1880)) {
+  if(document.documentElement.clientWidth>1186){
 
-    mybutton.style.display = "block";
-    
-  } else {
-    mybutton.style.display = "none";
+    if ((document.body.scrollTop > 200 && document.body.scrollTop < 1880)|| (document.documentElement.scrollTop > 200 && document.documentElement.scrollTop < 1880)) {
+  
+      mybutton.style.display = "block";
+      
+    } else {
+      mybutton.style.display = "none";
+    }
+  }else if(document.documentElement.clientWidth>763){
+    if ((document.body.scrollTop > 150 && document.body.scrollTop < 3031)|| (document.documentElement.scrollTop > 150 && document.documentElement.scrollTop < 3031)) {
+  
+      mybutton.style.display = "block";
+      
+    } else {
+      mybutton.style.display = "none";
+    }
+
+  }else{
+    if ((document.body.scrollTop > 100 && document.body.scrollTop < 5800)|| (document.documentElement.scrollTop > 200 && document.documentElement.scrollTop < 5800)) {
+  
+      mybutton.style.display = "block";
+      
+    } else {
+      mybutton.style.display = "none";
+    }
+
   }
 }
 
